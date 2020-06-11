6b0e3d2177001f23e4d06c630dfd080257f380a4
Khriz <0soundlive@gmail.com>
2020-06-10 23:14:54 -0500

    version 1
    maqueta 1

---
+++ PINKYLIN/server.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+const http = require('http');
+
+function webServer(req,res){
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end('<h1>Hola Node.js sin que te dieras cuenta escribi este codiguito</h1>');
+}
+
+http
+	.createServer(webServer)
+	.listen(3000,'localhost', () => console.log('Servidor corriendo en http://localhost:3000/'));
+
