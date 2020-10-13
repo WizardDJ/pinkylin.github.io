d81bca3fb3064405e656e020f45a316c37f3be05
Khriz <0soundlive@gmail.com>
2020-10-12 22:49:25 -0500

    act

--- js/brasier.js
+++ js/brasier.js
@@ -4,7 +4,7 @@ const brasier = [
 		image:"./img/brasier/brasier1.jpg",
 		title:"Brasier 814",
 		description:"Colores: Blanco, Negro, Grape, Verde Garden",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $58.000"
 	},
 	{
@@ -12,7 +12,7 @@ const brasier = [
 		image:"./img/brasier/brasier2.jpg",
 		title:"Brasier 744",
 		description:"Colores: Blanco, Negro, Grape, Nude, Rojo Candy",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $58.000"
 	},
 	{
@@ -20,7 +20,7 @@ const brasier = [
 		image:"./img/brasier/brasier3.jpg",
 		title:"Brasier 306",
 		description:"Colores: Blanco, Negro, Azulino, Merlot, Mauve, Verde garden, Nude, Beige, Rojo candy, Grape",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $45.000"
 	},
 	{
@@ -28,7 +28,7 @@ const brasier = [
 		image:"./img/brasier/brasier4.jpg",
 		title:"Brasier <br> Doble Realce 819",
 		description:"Colores: Blanco, Negro, Nude",
-		garment: "Talla: 32 - 34 - 36",
+		garment: "Talla: 32, 34, 36",
 		price: "Precio: $59.000 "
 	},
 	{
@@ -36,7 +36,7 @@ const brasier = [
 		image:"./img/brasier/brasier5.jpg",
 		title:"Brasier <br> Escote profundo 818",
 		description:"Colores: Blanco, Negro, Nude, Rojo Candy",
-		garment: "Talla: 32 - 34 - 36",
+		garment: "Talla: 32, 34, 36",
 		price: "Precio: $65.000 "
 	},
 	{
@@ -44,7 +44,7 @@ const brasier = [
 		image:"./img/brasier/brasier6.jpg",
 		title:"Brasier 815",
 		description:"Colores: Blanco, Negro, Azulino, Rojo Candy",
-		garment: "Talla: 32 - 34 - 36",
+		garment: "Talla: 32, 34, 36,",
 		price: "Precio: $60.000 "
 	},
 	{
@@ -52,7 +52,7 @@ const brasier = [
 		image:"./img/brasier/brasier7.jpg",
 		title:"Brasier 816",
 		description:"Colores: Blanco, Negro, Mauve, Beige, Verde Garden",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $59.000 "
 	},
 	{
@@ -60,7 +60,7 @@ const brasier = [
 		image:"./img/brasier/brasier8.jpg",
 		title:"Brasier 553",
 		description:"Colores: Blanco, Negro, Nude, Rojo Candy, Mauve, Grape",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $49.000 "
 	},	
 	{
@@ -68,7 +68,7 @@ const brasier = [
 		image:"./img/brasier/brasier9.jpg",
 		title:"Brasier <br> Doble Realce 645",
 		description:"Colores: Blanco, Negro, Nude, Azulino, Mauve, Grape",
-		garment: "Talla: 32 - 34 - 36",
+		garment: "Talla: 32, 34, 36",
 		price: "Precio: $50.000 "
 	},
 	{
@@ -76,7 +76,7 @@ const brasier = [
 		image:"./img/brasier/brasier10.jpg",
 		title:"Brasier 481",
 		description:"Colores: Blanco, Negro, Merlot</br>Mauve, Verde Garden, Grape",
-		garment: "Talla: 34 - 36 - 38 - 40",
+		garment: "Talla: 34, 36, 38, 40",
 		price: "Precio: $63.000 "
 	},
 	{
@@ -84,7 +84,7 @@ const brasier = [
 		image:"./img/brasier/brasier11.jpg",
 		title:"Brasier Strapless 822",
 		description:"Colores: Blanco, Negro, Nude",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $59.000 "
 	},
 	{
@@ -92,7 +92,7 @@ const brasier = [
 		image:"./img/brasier/brasier12.jpg",
 		title:"Brasier 790",
 		description:"Colores: Blanco, Negro, Nude, Grape",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $58.000 "
 	},
 	{
@@ -100,7 +100,7 @@ const brasier = [
 		image:"./img/brasier/brasier13.jpg",
 		title:"Brasier Soporte alto 652",
 		description:"Colores: Blanco, Negro, Nude, Verde Garden",
-		garment: "Talla: 34 - 36 - 38 - 40 - 42",
+		garment: "Talla: 34, 36, 38, 40, 42",
 		price: "Precio: $70.000 "
 	},
 	{
@@ -108,7 +108,7 @@ const brasier = [
 		image:"./img/brasier/brasier14.jpg",
 		title:"Brasier Soporte alto 820",
 		description:"Colores: Blanco, Negro, Beige, Merlot",
-		garment: "Talla: 34 - 36 - 38 - 40",
+		garment: "Talla: 34, 36, 38, 40",
 		price: "Precio: $70.000 "
 	},
 	{
@@ -116,7 +116,7 @@ const brasier = [
 		image:"./img/brasier/brasier15.jpg",
 		title:"Brasier Sin Copa 817",
 		description:"Colores: Negro, Beige",
-		garment: "Talla: 34 - 36 - 38 - 40",
+		garment: "Talla: 34, 36, 38, 40",
 		price: "Precio: $68.000 "
 	},
 	{
@@ -124,7 +124,7 @@ const brasier = [
 		image:"./img/brasier/brasier16.jpg",
 		title:"Brasier Básico 304",
 		description:"Colores: Blanco, Negro, Beige, Verde garden, Rojo candy, Grape, Azulino, Merlot, Nude",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $40.000 "
 	},
 	{
@@ -132,7 +132,7 @@ const brasier = [
 		image:"./img/brasier/brasier17.jpg",
 		title:"Brasier Strapless 648",
 		description:"Colores: Blanco, Negro, Beige",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $65.000 "
 	},
 	{
@@ -140,7 +140,7 @@ const brasier = [
 		image:"./img/brasier/brasier18.jpg",
 		title:"Brasier Soporte alto 653",
 		description:"Colores: Blanco, Negro, Beige, Azulino, Mauve",
-		garment: "Talla: 34 - 36 - 38 - 40 - 42",
+		garment: "Talla: 34, 36, 38, 40, 42",
 		price: "Precio: $69.000 "
 	},
 	{
@@ -148,7 +148,7 @@ const brasier = [
 		image:"./img/brasier/brasier19.jpg",
 		title:"Brasier 824",
 		description:"Colores: Blanco, Negro, Grape, Beige, Merlot",
-		garment: "Talla: 32 - 34 - 36 - 38",
+		garment: "Talla: 32, 34, 36, 38",
 		price: "Precio: $55.000 "
 	},
 	{
@@ -156,7 +156,7 @@ const brasier = [
 		image:"./img/brasier/brasier20.jpg",
 		title:"Brasier 486",
 		description:"Colores: Blanco, Negro, Merlot </br>Mauve, Verde Garden, Grape",
-		garment: "Talla: 34 - 36 - 38 - 40",
+		garment: "Talla: 34, 36, 38, 40",
 		price: "Precio: $58.000 "
 	},
 	
