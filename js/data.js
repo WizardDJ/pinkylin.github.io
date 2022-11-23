94dd41552ccb74b0e30340bb20461fc98b80a484
CRKJ <0soundlive@gmail.com>
2022-11-23 17:06:05 -0500

    lenceria

--- js/data.js
+++ js/data.js
@@ -1,11 +1,67 @@
 const lenceria = [
 	{
 		id:0,
-		image:"./img/lenceria/lenceria1.jpg",
-		title:"Lencería cachetero",
-		description:"Ref: DNL01 Colores surtidos",
-		garment:"Tallas: S M L",
-		price:"Precio: $ 60.000"
+		image:"./img/lenceria/903.jpg",
+		title:"Body Manga Larga 903",
+		description:"Negro, Champan, Tawny",
+		garment:"Tallas: 34, 36, 38",
+		price:"Precio: $ 115.000"
+	},
+	{
+		id:1,
+		image:"./img/lenceria/974.jpg",
+		title:"Body en blonda 974",
+		description:"Nude, Negro",
+		garment:"Tallas: 32, 34, 36, 38",
+		price:"Precio: $ 110.000"
+	},
+	{
+		id:2,
+		image:"./img/lenceria/975.jpg",
+		title:"Body Manga Larga 975",
+		description:"Negro",
+		garment:"Tallas: S, M, L, XL",
+		price:"Precio: $ 88.000"
+	},
+	{
+		id:3,
+		image:"./img/lenceria/322.jpg",
+		title:"Corsette en blonda 322",
+		description:"Incluye panty hilo <br> Blanco, Negro, Rojo Candy",
+		garment:"Tallas: 32, 34, 36, 38",
+		price:"Precio: $ 125.000"
+	},
+	{
+		id:4,
+		image:"./img/lenceria/976.jpg",
+		title:"Corselette 976",
+		description:"Negro, Rojo Candy",
+		garment:"Tallas: 32, 34, 36, 38",
+		price:"Precio: $ 110.000"
+	},
+	{
+		id:5,
+		image:"./img/lenceria/977.jpg",
+		title:"Teddy sensual 977",
+		description:"Negro, Rojo Candy",
+		garment:"Tallas: S, M, L",
+		price:"Precio: $ 75.000"
+	},
+	{
+		id:6,
+		image:"./img/lenceria/978.jpg",
+		title:"Conjunto 978",
+		description:" Conjunto sensual 3 piezas, <br> Negro, Rojo Candy",
+		garment:"Tallas: 32, 34, 36",
+		price:"Precio: $ 130.000"
+	},
+	{
+		id:7,
+		image:"./img/lenceria/678.jpg",
+		title:"Babydoll 678",
+		description:" Babydoll en satin <br> Blanco, Negro, Rojo Candy",
+		garment:"Tallas: 32, 34, 36, 38",
+		price:"Precio: $ 90.000"
 	},
 ];
 
@@ -45,11 +101,11 @@ const panty = [
 const playa = [
 	{
 		id:0,
-		image:"./img/medias/0.jpg",
-		title:"Medias térmicas",
-		description:"Media exclusiva de animalitos, varios motivos, térmicas.",
-		garment:"",
-		price:"Precio: $15.000"
+		image:"./img/playa/7126-7111.jpg",
+		title:"Vestido de baño 7126 <br> Salida 7111",
+		description:"Vestido de baño 7126 <br> Tallas: 32, 34, 36, 38 ",
+		garment:"Salida 7111, Talla SM-ML",
+		price:"Precio: $135.000 y $85.000"
 	},
 ];
 
