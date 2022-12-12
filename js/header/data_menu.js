31a0ae98c10c736cecdb0b3b819370b6f9553659
CRKJ <0soundlive@gmail.com>
2022-12-12 14:15:19 -0500

    3 seccion actualizada

--- js/header/data_menu.js
+++ js/header/data_menu.js
@@ -1,1 +1,10 @@
-export const data_menu = ['Playa', 'Lenceria', 'index', 'Panty', 'Brasier', 'Pijama', 'Caballero', 'Pinkylin']
+let item1 = 'Playa'; 
+let item2 = 'Lenceria'; 
+let item3 = 'index'; 
+let item4 = 'Panty'; 
+let item5 = 'Brasier'; 
+let item6 = 'Pijamas'; 
+let item7 = 'Caballero'; 
+let item8 = 'Pinkylin'; 
+
+export const data_menu = [item1, item2, item3, item8]
