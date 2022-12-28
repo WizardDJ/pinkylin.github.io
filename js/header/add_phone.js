d40cb6b39453a179c08f3fece341a952d0560464
CRKJ <0soundlive@gmail.com>
2022-12-28 16:12:00 -0500

    barra en blanco en desktop

--- js/header/add_phone.js
+++ js/header/add_phone.js
@@ -7,8 +7,8 @@ export const add_phone = document.createElement('div')
 
 add_phone.className = 'whatsapp_number'
 add_phone.innerHTML = `
-
-  <a href="${whatsapp1}" target="_blank"> 
+<div class="whatsapp_text">Whatsapp</div>
+<a href="${whatsapp1}" target="_blank"> 
     <span class="icon-whatsapp whatsapp wm"></span> ${phone1}
   </a>
 
